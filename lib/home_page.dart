3b74b9bb11b02ad0e085f3c9758ea30d650dc529
yash <yashmakwana40@gmail.com>
2022-01-18 21:22:43 +0530

    Day 2 Completed

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("First"),
+        backgroundColor: Colors.green,
+        shadowColor: Colors.black26,
+        titleSpacing: 100,
+      ),
+      body:  Center(
+                child: Container(
+          child: Text("Welcome to flutter",
+           ),
+          ),
+        ),
+        drawer: Drawer(),
+    );
+  }
+}
